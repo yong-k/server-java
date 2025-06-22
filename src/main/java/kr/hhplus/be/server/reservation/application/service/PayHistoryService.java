d365ba1b5b5a19de8ffa79798622e2b603cd4e1f
jeongyong <cin03023@gmail.com>
2025-06-22 23:42:50 +0900

    fix: @Transactional 누락 추가

--- src/main/java/kr/hhplus/be/server/reservation/application/service/PayHistoryService.java
+++ src/main/java/kr/hhplus/be/server/reservation/application/service/PayHistoryService.java
@@ -11,6 +11,8 @@ import kr.hhplus.be.server.user.UserRepository;
 import kr.hhplus.be.server.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -23,6 +25,7 @@ public class PayHistoryService implements PayHistoryUseCase {
 
     // 성공이력은 기존 트랜잭션 롤백 시, 함께 롤백되어야 하므로 REQUIRED
     @Override
+    @Transactional
     public void saveSuccessHistory(Seat seat, UUID userId, PaymentStatus status, PaymentReason reason) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new DataNotFoundException("사용자가 존재하지 않습니다: userId = " + userId));
@@ -32,6 +35,7 @@ public class PayHistoryService implements PayHistoryUseCase {
 
     // 예외 상황에서도 결제 실패 이력을 반드시 남기기 위한 REQUIRES_NEW 트랜잭션 분리
     @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void saveFailedHistory(Seat seat, UUID userId, PaymentStatus status, PaymentReason reason) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new DataNotFoundException("사용자가 존재하지 않습니다: userId = " + userId));
